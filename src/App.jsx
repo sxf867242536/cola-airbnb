a4b15c6c4a8c3de6d52fa4afd6fec9166115c209
Sun.xf <867242536@qq.com>
2022-10-11 22:36:20 +0800

    init(tailwindcss): 新增tailwindcss依赖

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React, { memo } from 'react';
 
 const App = memo(() => (
-  <div>App</div>
+  <div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
 ));
 
 export default App;
